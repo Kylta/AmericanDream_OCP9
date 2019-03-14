69d552620eb2b1c8b4760e372a1824e16a562aa0
Kylta <Christophe.bugnon02@gmail.com>
2019-03-14 16:50:31 +0100

    Create AmericanDream project

---
+++ AmericanDream/AmericanDreamTests/AmericanDreamTests.swift
@@ -0,0 +1,34 @@
+//
+//  AmericanDreamTests.swift
+//  AmericanDreamTests
+//
+//  Created by Christophe Bugnon on 14/03/2019.
+//  Copyright © 2019 Christophe Bugnon. All rights reserved.
+//
+
+import XCTest
+@testable import AmericanDream
+
+class AmericanDreamTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
