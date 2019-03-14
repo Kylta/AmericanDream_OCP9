a621dbbb6eb64580b7124dd0e2d95c07d47e010c
Kylta <Christophe.bugnon02@gmail.com>
2019-03-14 17:46:32 +0100

    Add UIPicker delegate & datasource

--- AmericanDream/AmericanDream/TranslatorController.swift
+++ AmericanDream/AmericanDream/TranslatorController.swift
@@ -11,12 +11,17 @@ import UIKit
 class TranslatorController: UIViewController {
 
     let translatorView = TranslatorView()
+    let model = TranslateModel(translatedText: "")
+    var languageTranslation = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .green
         setupUI()
+
+        translatorView.pickerBotLanguage.delegate = self
+        translatorView.pickerBotLanguage.dataSource = self
     }
 
     private func setupUI() {
@@ -26,5 +31,33 @@ class TranslatorController: UIViewController {
          translatorView.leftAnchor.constraint(equalTo: view.leftAnchor),
          translatorView.rightAnchor.constraint(equalTo: view.rightAnchor),
          translatorView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)].forEach({$0.isActive = true})
+    }
+}
+
+extension TranslatorController: UIPickerViewDelegate, UIPickerViewDataSource {
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return model.data.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return CGFloat(40)
+    }
+
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let pickerLabelBot = UILabel()
+        pickerLabelBot.text = model.data[row]
+        pickerLabelBot.textAlignment = .center
+
+        if let text = pickerLabelBot.text {
+            languageTranslation = text
+            return pickerLabelBot
+        }
+
+        return pickerLabelBot
     }
 }
--- AmericanDream/AmericanDream/TranslatorItem.swift
+++ AmericanDream/AmericanDream/TranslatorItem.swift
@@ -14,6 +14,10 @@ public struct TranslateModel: Equatable {
     public init(translatedText: String) {
         self.translatedText = translatedText
     }
+
+    public var data: [String] = {
+        return ["af","sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb", "zh-CN", "zh-TW", "co", "hr", "cs", "da", "nl", "en", "eo", "et", "fi", "fr", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha", "haw", "iw", "hi", "hmn", "hu", "is", "ig", "id", "ga", "it", "ja", "jw", "kn", "kk", "km", "ko", "ku", "ky", "lo", "la", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no", "ny", "ps", "fa", "pl", "pt", "pa", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tl", "tg", "ta", "te", "th", "th", "uk", "ur", "uz", "vi", "cy", "xh", "yi", "yo", "zu" ].sorted(by: {$0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending})
+    }()
 }
 
 internal final class TranslateItemMapper: Decodable {
