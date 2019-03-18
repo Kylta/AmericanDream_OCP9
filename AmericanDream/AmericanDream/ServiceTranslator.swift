74957d5a066be77a1ca276cb779a2e3da11dcb2c
Kylta <Christophe.bugnon02@gmail.com>
2019-03-18 11:42:53 +0100

    Fix files bug

---
+++ AmericanDream/AmericanDream/ServiceTranslator.swift
@@ -0,0 +1,35 @@
+//
+//  ServiceTranslator.swift
+//  AmericanDream
+//
+//  Created by Christophe Bugnon on 18/03/2019.
+//  Copyright © 2019 Christophe Bugnon. All rights reserved.
+//
+
+import Foundation
+
+struct ServiceTranslator {
+
+    static let shared = ServiceTranslator()
+
+    private init() {}
+
+    func downloadFromServer(text: String, languageTranslation: String, completionHandler: @escaping (TranslateModel) -> ()) {
+        let originalString = "https:www.googleapis.com/language/translate/v2?key=AIzaSyAZBRy5yETaiffUODxICqMxGBrSnSWXzRQ&target=\(languageTranslation)&q=\(text)"
+        let urlString = originalString.components(separatedBy: .whitespaces).joined()
+
+        guard let url = URL(string: urlString) else { return }
+
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let err = error {
+                print("Failed to fetch from server:", err)
+            }
+
+            guard let data = data, let response = response as? HTTPURLResponse else { return }
+
+            if let jsonTranslator = TranslateItemMapper.map(data, response) {
+                completionHandler(jsonTranslator)
+            }
+            }.resume()
+    }
+}
